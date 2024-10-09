8827f6b359e3cdf2901dcd27e18a01302dbe8235
PsProfi <you@example.com>
2024-10-09 13:35:08 +0300

    lp

---
+++ web/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  Map<String, dynamic> person = {
+    "name": "Павло",
+    "age": 15,
+    "hobby": "ліжколежання"
+  };
+
+  print("Ім'я: ${person['name']}");
+  print("Вік: ${person['age']}");
+  print("Хобі: ${person['hobby']}");
+}
